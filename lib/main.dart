b07f20506a2d50025d3cafaea167d78006a12efc
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-07-26 20:18:24 +0530

    second commit duh

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,33 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      title: "bluer",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Center(
+          child: Text("this is the first screen yay!!"),
+        ),
+      ),
+    );
   }
 }
